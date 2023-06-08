a1da32cd2e144166b0a28bf8336b3766112b3ce4
onzerebrian <onzerebrian@gmail.com>
2023-06-08 05:03:47 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
+a = 1
+b = 2
 if __name__ == "__main__":
-    b = 2
-    a = 1
-    c = add(a,b)
-    print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, add(a,b)))
