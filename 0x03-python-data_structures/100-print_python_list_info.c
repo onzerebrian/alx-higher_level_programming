b725423eabd4c29b6c567eb4823c280d2eee1ae3
onzerebrian <onzerebrian@gmail.com>
2023-06-09 06:09:47 -0700

    tasks

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to python object
+ */
+
+
+void print_python_list_info(PyObject *p)
+{
+
+
+}
