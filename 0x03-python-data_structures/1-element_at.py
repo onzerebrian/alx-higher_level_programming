f62c2ca00199595f7716cf2ec894a64aa4f10bc9
onzerebrian <onzerebrian@gmail.com>
2023-06-12 06:27:25 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    i = 0
+    for m in my_list:
+        if i == idx:
+            return m
+        i++
