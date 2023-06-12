64563096394ce387422ce020015616439719af18
onzerebrian <onzerebrian@gmail.com>
2023-06-12 09:56:19 +0000

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def max_integer(my_list=[]):
+    if my_list[0] == None:
+        return None
+    max_va = my_list[0]
+    for i in my_list:
+        if max_va < i:
+            max_va = i
+    return max_va
