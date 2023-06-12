4d10849a3db5e9c1276a6f822abd92e835fd5439
onzerebrian <onzerebrian@gmail.com>
2023-06-12 10:02:23 +0000

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    ar = []
+    for i in my_list:
+        if (i % 2 == 0):
+            ar.append(True)
+        else:
+            ar.append(False)
+    return (ar)
