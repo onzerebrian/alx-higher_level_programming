7116a027f9f8ecc96ab2f13fc0788ab6f1d7cdfa
onzerebrian <onzerebrian@gmail.com>
2023-06-12 07:08:55 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,7 +7,7 @@ def replace_in_list(my_list, idx, element):
     i = 0
     for m in my_list:
         if i == idx:
-            my_list.remove(i)
             my_list.insert(i, element)
+            my_list.remove(i + 1)
             return my_list
         i += 1
