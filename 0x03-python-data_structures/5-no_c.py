c0c8b3fe109d07c4ff6379cd1e88604d10bcb480
onzerebrian <onzerebrian@gmail.com>
2023-06-12 08:31:36 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    lt = []
-    for m in my_string:
-        lt.append(m)
-    for i in lt:
-        if (i == 'c') or (i == 'C'):
-            lt.remove(i)
-    my_string = ""
-    x = 0
-    for i in lt:
-        my_string = my_string + i
-    return my_string
+    my_string2 = ""
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            my_string2 = my_string2 + i
+    return my_string2
