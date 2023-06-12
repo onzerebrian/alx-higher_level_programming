9012da87681bf221fe6c1739997e0df43112b0f1
onzerebrian <onzerebrian@gmail.com>
2023-06-12 07:44:53 +0000

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+def new_in_list(my_list, idx, element):
+    m = []
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    for i in my_list:
+        m.append(i)
+    m.insert(idx, element)
+    m.remove(idx + 1)
+    return m
