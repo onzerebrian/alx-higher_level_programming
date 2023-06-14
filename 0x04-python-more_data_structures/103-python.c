5362ab94578689fb298d6ee411a80182f8449201
onzerebrian <onzerebrian@gmail.com>
2023-06-14 08:09:17 +0000

    tak 103

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_python_list - function
+ * @p: pointer
+*/
+void print_python_list(PyObject *p)
+{
+
+
+}
+
+/**
+ * print_python_bytes - function
+ * @p: pointer
+*/
+void print_python_bytes(PyObject *p)
+{
+
+}
