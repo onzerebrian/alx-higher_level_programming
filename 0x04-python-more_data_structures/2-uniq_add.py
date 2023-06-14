00d698fd683e3b1a3cd97d9e3322c9f64749330c
onzerebrian <onzerebrian@gmail.com>
2023-06-14 10:54:11 +0000

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-
+def uniq_add(my_list=[]):
+    _uniq=[]
+    for i in my_list:
+        x = i
+        v = 0
+        for m in _uniq:
+            if m == x:
+                v += 1
+        if v == 0:
+            _uniq.append(i)
+    s_um = 0
+    for i in _uniq:
+        s_um += i
+    return s_um
