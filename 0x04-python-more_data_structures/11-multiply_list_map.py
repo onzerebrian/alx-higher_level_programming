069a4139657b46e7820c0e8656ce6601bded80df
onzerebrian <onzerebrian@gmail.com>
2023-06-14 14:28:22 +0000

    task 12

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda yy: yy*number, my_list))
+
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
-        return 0
-    for i in roman_string:
-        if i is "I":
-            i = 1
-        if i is "V":
-            i = 5
-        if i is "X":
-            i = 10
-        if i is "L":
-            i = 50
-        if i is "C":
-            i = 100
-        if i is "D":
-            i = 500
-        if i is "M":
-            i = 1000
-        add = sum(roman_string[i])
-    return add
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    if (type(roman_string) is not str or roman_string is None):
+        return (0)
+    for i, char in enumerate(roman_string):
+        tmp_num = roman[char]
+        try:
+            if tmp_num < roman[roman_string[i + 1]]:
+                tmp_num = tmp_num * -1
+        except IndexError:
+            pass
+        number = number + tmp_num
+    return (number)
