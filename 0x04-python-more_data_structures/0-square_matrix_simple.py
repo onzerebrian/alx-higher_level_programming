4652428cee48198d351d02377574454bf1078e77
onzerebrian <onzerebrian@gmail.com>
2023-06-14 09:01:42 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
 
+    for i in matrix:
+        new_row = []
+        for j in i:
+            new_row.append(j*j)
+        new_matrix.append(new_row)
+    return new_matrix
