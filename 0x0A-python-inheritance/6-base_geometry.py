ff7e649d5d990e62c6fae68838cb6c622661918e
onzerebrian <onzerebrian@gmail.com>
2023-07-10 17:02:06 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains one class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ contains one public instance method """
+    def area(self):
+        """
+        raises an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
