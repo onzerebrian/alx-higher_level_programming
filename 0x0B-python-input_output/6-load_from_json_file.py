50e7cbfb914a48892b258c133e5f9357ee6f3361
onzerebrian <onzerebrian@gmail.com>
2023-07-11 16:22:08 +0300

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,9 +5,8 @@ This module contains one function
 import json
 
 
-def from_json_string(my_str):
-    """
-    returns an object (Python data structure)
-    represented by a JSON string
-    """
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using a JSON representation """
+    with open(filename, 'w') as f:
+        j = json.dumps(my_obj)
+        f.write(j)
