e8b45e231e46501dbaaebc56b5a9e4ee0e3e1280
onzerebrian <onzerebrian@gmail.com>
2023-07-11 16:39:57 +0300

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """
-a script that adds all arguments to a Python list, and then save them to a file
+This module contains one class
 """
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-try:
-    obj = load_from_json_file("add_item.json")
-except:
-    save_to_json_file([], "add_item.json")
-obj = load_from_json_file("add_item.json")
-l = []
-for i in range(1, len(sys.argv)):
-    l.append(sys.argv[i])
-obj = obj + l
-save_to_json_file(obj, "add_item.json")
+
+
+class Student:
+    """
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance """
+        return self.__dict__
