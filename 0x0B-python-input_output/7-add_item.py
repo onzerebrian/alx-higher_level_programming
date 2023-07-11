9ff354d31e1e0374d295c1ae55f514d0a2f4fae9
onzerebrian <onzerebrian@gmail.com>
2023-07-11 16:25:57 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains one function
+a script that adds all arguments to a Python list, and then save them to a file
 """
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """ writes an Object to a text file, using a JSON representation """
-    with open(filename, 'w') as f:
-        j = json.dumps(my_obj)
-        f.write(j)
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+try:
+    obj = load_from_json_file("add_item.json")
+except:
+    save_to_json_file([], "add_item.json")
+obj = load_from_json_file("add_item.json")
+l = []
+for i in range(1, len(sys.argv)):
+    l.append(sys.argv[i])
+obj = obj + l
+save_to_json_file(obj, "add_item.json")
