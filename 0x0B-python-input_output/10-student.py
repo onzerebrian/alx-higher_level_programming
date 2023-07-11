e3546b5b9badaeb53d8d28c46a2aaec81a2574d3
onzerebrian <onzerebrian@gmail.com>
2023-07-11 16:51:46 +0300

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """
-This module contains one function
+This module contains one class
 """
 
 
-def class_to_json(obj):
+class Student:
     """
-    returns the dictionary description
-    with simple data structure
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object
+    Public instance attributes:
+    first_name
+    last_name
+    age
     """
-    return obj.__dict__
+    def __init__(self, first_name, last_name, age):
+        """ instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        else:
+            d = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    d[i] = self.__dict__[i]
+            return d
