bb23fcb31e449faae5d7dce7c38bcff66c74f8d0
onzerebrian <onzerebrian@gmail.com>
2023-07-11 16:04:00 +0300

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contains one function
+"""
 
+
+def number_of_lines(filename=""):
+    """ returns the number of lines of a text file """
+    with open(filename) as f:
+        c = 0
+        for line in f:
+            c += 1
+    return c
