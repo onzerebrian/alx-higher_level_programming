95daaddfa6693b15bed58035ac47078ec8eee333
onzerebrian <onzerebrian@gmail.com>
2023-07-17 18:25:30 +0000

    rect test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_model.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_model.py
@@ -1,3 +1,36 @@
 #!/usr/bin/python3
+from models.rectangle import Rectangle
+import unittest
+
+"""
+rectangle tests
+
+"""
+
+class test_rectangle(unittest.TestCase):
+    '''
+        Testing rectangle
+    '''
+
+    def test_width(self):
+        '''
+            Testing the Rectangle width getter
+        '''
+        self.assertEqual(5, self.r.width)
+
+    def test_height(self):
+        '''
+            Testing the Rectangle height getter
+        '''
+        self.assertEqual(10, self.r.height)
+
+    def test_x(self):
+        '''
+            Testing Rectangle x getter and setter
+        '''
+
+        self.r.x = 54
+        self.assertEqual(54, self.r.x)
+        self.assertEqual(0, self.r.y)
 
 
