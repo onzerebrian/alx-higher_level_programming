24ba5feee6a50772592f5125a9abfeaf7a937661
onzerebrian <onzerebrian@gmail.com>
2023-07-17 18:02:10 +0000

    bas

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 
+"""
+base module
+
+"""
+
+class Base:
+    """
+    Base class
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        :param id:
+        """
+        if id is None:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
+
+        else:
+            self.id = id
+
