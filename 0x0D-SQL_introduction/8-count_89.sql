bdb3a042c6decddd834a3fa734420c686ab7f593
brianonzere <brianonzere18@gmail.com>
2023-08-17 09:25:00 +0300

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
--- Count all rows with id = 89
-SELECT COUNT(*) FROM `first_table` WHERE `id` = 8
+-- a script that displays the number of records with id = 89
+-- in the table first_table in the current database in the MySQL server
+SELECT COUNT(*) FROM first_table WHERE id = 89
